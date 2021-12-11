0f3a5496f6894a809a6c308c62a20c0f16665515
Irosi1997 <97itpeiris@gmail.com>
2021-12-11 12:38:17 +0530

    Commit App Theme
    Commit App Theme - with Lato font

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:demo/views/home.dart';
 import 'package:demo/common/route_generator.dart';
+import 'package:demo/common/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,16 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      title: 'Flutter Demo-Fonts',
+       theme: lightTheme,
+      // ThemeData(
+      //   primarySwatch: Colors.blue,
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
       //Define Route Map for Named Routes
       initialRoute: '/',
       //Instead of route map we have set generate route function using onGenerateRoute
       onGenerateRoute: RouteGenerator.generateRoute,
-      home: HomePage(title: 'Flutter Routing Example'),
+      home: HomePage(title: 'Flutter Fonts Example'),
     );
   }
 }
