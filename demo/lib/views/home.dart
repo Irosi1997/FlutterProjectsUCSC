56dc0aa2ff587b9fa214af43ce97cdfb3542c542
Irosi Peiris <84384704+Irosi1997@users.noreply.github.com>
2021-12-25 12:42:19 +0530

    Update home.dart

--- demo/lib/views/home.dart
+++ demo/lib/views/home.dart
@@ -35,6 +35,23 @@ class _HomePageState extends State<HomePage> {
               textColor: Colors.blue,
               padding: EdgeInsets.all(8.0),
               onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.blue)),
+              color: Colors.white,
+              textColor: Colors.blue,
+              padding: EdgeInsets.all(8.0),
+              onPressed: () {
                 Navigator.pushNamed(context, '/users');
               },
               child: Text(
