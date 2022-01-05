b3156ab1c15d4d2a29558aca708f0d480688582d
Irosi <97itpeiris@gmail.com>
2022-01-05 18:33:10 +0530

    Week 4 - Activity 1
    Week 4 - Activity 1

--- demo/lib/views/login.dart
+++ demo/lib/views/login.dart
@@ -108,9 +108,9 @@ class _LoginPageState extends State<LoginPage> with ValidationMixin {
   void logUser(context, username, password) async {
     var res = await login(context, username, password);
     if (res != null) {
-      var status = await Utilities.saveToken(res.accessToken);
+      Utilities.saveToken(res.accessToken);
       var token = await Utilities.userToken();
-      Navigator.pushReplacementNamed(context, '/home');
+      Navigator.pushNamed(context, '/users');
     }
   }
 }
